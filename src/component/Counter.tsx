54e3dddd9798cd7f3f421642ef068f1167663f0a
chiludiep <chiludiep@hotmail.com>
2022-10-03 11:27:26 -0400

    Styling

--- src/component/Counter.tsx
+++ src/component/Counter.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction, useState } from "react";
+import styles from "./Counter.module.scss";
 
 function CounterComponent() {
   const [counter1, setCounter1] = useState<number>(0);
@@ -9,8 +10,16 @@ function CounterComponent() {
   const sum1 = isDisabled1 ? 0 : counter1;
   const sum2 = isDisabled2 ? 0 : counter2;
 
+  const sumMarkup = (
+    <div className={styles.Sum}>
+      <h2>Sum</h2>
+      <p> {sum1 + sum2}</p>
+    </div>
+  );
+
   return (
     <>
+      {sumMarkup}
       <Counter
         counter={counter1}
         isDisabled={isDisabled1}
@@ -24,7 +33,6 @@ function CounterComponent() {
         setCounter={setCounter2}
         setisDisabled={setisDisabled2}
       />
-      <p>Sum: {sum1 + sum2}</p>
     </>
   );
 }
@@ -43,15 +51,23 @@ function Counter({
   setisDisabled,
 }: CounterProps) {
   return (
-    <div style={{ display: "flex", gap: "1em" }}>
-      <button disabled={isDisabled} onClick={() => decrement()}>
+    <div className={styles.CounterContainer}>
+      <button
+        className={styles.Button}
+        disabled={isDisabled}
+        onClick={() => decrement()}
+      >
         -
       </button>
       <p>{counter}</p>
-      <button disabled={isDisabled} onClick={() => increment()}>
+      <button
+        className={styles.Button}
+        disabled={isDisabled}
+        onClick={() => increment()}
+      >
         +
       </button>
-      <button onClick={() => handleDisable()}>
+      <button className={styles.Button} onClick={() => handleDisable()}>
         {isDisabled ? "Enable" : "Disable"}
       </button>
     </div>
