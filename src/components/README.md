934177edb833a35066f2d8a2a023a7ca475657a3
chiludiep <chiludiep@hotmail.com>
2022-10-27 18:56:26 -0400

    npm

---
+++ src/components/README.md
@@ -0,0 +1,7 @@
+# Simplisty
+
+A React component library made so you can build clean, simple & minimalist UI's
+
+## Install and usage
+
+1. `npm install simplisty-component`
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Button } from "./Button";
+import { Card } from "./Card";
+import { Counter } from "./Counter";
+import { Select } from "./Select";
+
+export { Button, Card, Counter, Select };
